ca35aa939d4bbb424568e6a39dd7072d5f7b380b
Lucas Moura <lucasmoura.developer@hotmail.com>
2019-10-05 00:50:11 -0300

    feature: created user session

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import api from './services/api';
+import './App.css';
 
-import logo from "./assets/logo.svg";
+import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
 
       <div className="content">
         <p>
-          Ofereça <strong>spots</strong> para programadores e encontre{" "}
+          Ofereça <strong>spots</strong> para programadores e encontre{' '}
           <strong>talentos</strong> para sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
-          <input id="email" type="email" placeholder="Seu melhor e-mail" />
+          <input
+            id="email"
+            type="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
 
           <button className="btn" type="submit">
             Entrar
